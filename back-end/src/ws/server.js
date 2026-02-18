ff1e3dd67e80d6e73de83326983436d9f2a82b3a
Subham Mondal <subhamoffi@gmail.com>
2026-02-18 22:21:31 +0530

    implemented coderabbit changes

--- back-end/src/ws/server.js
+++ back-end/src/ws/server.js
@@ -17,26 +17,33 @@ export function broadcastMessage(wss, payload) {
 export function attachWebsocketServer(server) {
     const wss = new WebSocketServer({ server, path: "/ws", maxPayload: 1024 * 1024 });
 
-    wss.on("connection", async (socket, req) => {
+    server.on("upgrade", async (req, socket, head) => {
+        const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+        if (pathname !== "/ws") return;
         if (wsArcjet) {
             try {
                 const descision = await wsArcjet.protect(req);
                 if (descision.isDenied()) {
-                    const code = descision.reason.isRateLimit() ? 1013 : 1008;
-                    const reason = descision.reason.isRateLimit()
-                        ? "Rate limit exceeded"
-                        : "Access denied";
-
-                    socket.close(code, reason);
-                    return;
+                    socket.write("HTTP/1/1 429 Too Many Request\r\n\r\n");
+                } else {
+                    socket.write("HTTP/1/1 403 Forbidden\r\n\r\n");
                 }
+                socket.destroy();
+                return;
             } catch (e) {
                 console.error("WS connection error", e);
-                socket.close(1011, "Server security error");
+                socket.write("HTTP/1/1 500 Internal Server Error\r\n\r\n");
+                socket.destroy();
                 return;
             }
         }
 
+        wss.handleUpgrade(req, socket, head, (ws) => {
+            wss.emit("connection", ws, req);
+        });
+    });
+
+    wss.on("connection", async (socket, req) => {
         socket.isAlive = true;
 
         socket.on("pong", () => {
